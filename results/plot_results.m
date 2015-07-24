7a0c5f2ddd43743b09ae3558778dee9613c0c002
Seyed Vahid Azhari <azharivs@iust.ac.ir>
2015-07-24 14:03:26 +0430

    user: Seyed Vahid Azhari <azharivs@iust.ac.ir>  branch 'Fakhralis'  added results/extract-all.sh  added results/extract-sta-agg-stat.sh  changed results/plot_results.m  changed scratch/sva-simple-mpdu-aggregation.cc  changed src/wifi/model/mac-low.cc  changed src/wifi/model/mpdu-universal-aggregator.cc >>> Added trace for     Aggregation size     Tx Time     Data Rate

--- results/plot_results.m
+++ results/plot_results.m
@@ -1,7 +1,7 @@
 close all;
 clear all;
 nSta = 4;
-baseLogName = 'logfiles/log_mimo_channel.1';
+baseLogName = 'logfiles/log_mimo_channel.4';
 pattern = {'k-','r-','g-','m-','b-','y-'};
 bssPhyMacLogName = sprintf('%s.BssPhyMacStats',baseLogName);
 for i=1:nSta
@@ -11,6 +11,7 @@ for i=1:nSta
         mac = sprintf('00:00:00:00:00:%X',i);     
     end
     staQInfoLogName{i} = sprintf('%s.StaQInfo.%s',baseLogName,mac);
+    staAggLogName{i} = sprintf('%s.StaAgg.%s',baseLogName,mac);
     legendStr{i} = sprintf('STA %d',i);
 end
 
@@ -28,9 +29,77 @@ xlabel('Time (seconds)');
 ylabel('milli-seconds');
 grid;
 
-% Process StaQInfo data
+% Process StaQInfo and aggregation data
 figure;
 for i=1:nSta
+    %Sta Q Info
+    data = load(staQInfoLogName{i});
+    times = data(:,1);
+    avgQPkt = data(:,4);
+    avgQWait = data(:,6);
+    avgQArrivalMbps = data(:,8);
+    avgQDvp = data(:,9);
+    clear data;
+    subplot(3,3,1);
+    plot(times,avgQPkt,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Avg. Queue Length (pkt)');
+    grid on;
+    subplot(3,3,2);
+    plot(times,avgQWait,pattern{i});
+    hold on;
+    xlabel('Time (seconds)');
+    ylabel('Avg. Waiting Time (msec)');
+    grid on;
+    subplot(3,3,3);
+    plot(times,avgQArrivalMbps,pattern{i});
+    hold on;
+    xlabel('Time (seconds)');
+    ylabel('Avg. Arrival Rate (Mb/s)');
+    grid on;
+    subplot(3,3,4);
+    plot(times,avgQDvp,pattern{i});
+    hold on;
+    xlabel('Time (seconds)');
+    ylabel('Delay Violation Prob.');
+    grid on;
+    
+    %Aggregation info
+    data = load(staAggLogName{i});
+    times = data(:,1);
+    aggPkts = data(:,3);
+    dataRate = data(:,4);
+    aggTxTime = data(:,5);
+    clear data;
+    subplot(3,3,5);
+    plot(times,aggPkts,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Size of A-MPDU (packets)');
+    grid on;
+    subplot(3,3,6);
+    plot(times,aggTxTime,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Tx Time of A-MPDU (msec)');
+    grid on;
+    subplot(3,3,7);
+    plot(times,dataRate,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Data Rate (Mb/s)');
+    grid on;    
+end
+
+% Repeat StaQInfo in separate plots
+figure;
+for i=1:nSta
+    %Sta Q Info
     data = load(staQInfoLogName{i});
     times = data(:,1);
     avgQPkt = data(:,4);
@@ -64,3 +133,39 @@ for i=1:nSta
     ylabel('Delay Violation Prob.');
     grid on;
 end
+
+
+
+% Repeat aggregation info in separate plot
+figure;
+for i=1:nSta
+    %Aggregation info
+    data = load(staAggLogName{i});
+    times = data(:,1);
+    aggPkts = data(:,3);
+    dataRate = data(:,4);
+    aggTxTime = data(:,5);
+    clear data;
+    subplot(2,2,1);
+    plot(times,aggPkts,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Size of A-MPDU (packets)');
+    grid on;
+    subplot(2,2,2);
+    plot(times,aggTxTime,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Tx Time of A-MPDU (msec)');
+    grid on;
+    subplot(2,2,3);
+    plot(times,dataRate,pattern{i});
+    hold on;
+    legend(legendStr);
+    xlabel('Time (seconds)');
+    ylabel('Data Rate (Mb/s)');
+    grid on;    
+end
+
